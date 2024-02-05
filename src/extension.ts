74e62e54656dc730099c6ddf764de753070da499
wufan12 <wufan12@kuaishou.com>
2024-02-05 10:59:36 +0800

    feat: 提交

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('ddd.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from demo-plugin-letter!');
+		vscode.window.showInformationMessage('Hello World from demo-plugin-letter 222!');
 	});
 
 	context.subscriptions.push(disposable);
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export interface SubConfigType {
+  openType: 'webview' | 'theme' | 'debugger' | 'default'
+}
